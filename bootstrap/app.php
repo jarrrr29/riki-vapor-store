a0e0f3ee290e25fba598993aac9a65e9f2fcd146
jarrrr29 <138673167+jarrrr29@users.noreply.github.com>
2025-07-22 04:03:01 +0700

    Implement Vercel writable tmp directory strategy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,9 @@
 <?php
 
+    if (isset($_ENV['VERCEL'])) {
+    require __DIR__ . '/vercel.php';
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,3 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+
+
+
+
+
---
+++ bootstrap/vercel.php
@@ -0,0 +1,12 @@
+<?php
+
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
+$_ENV['CACHE_DRIVER'] = 'array';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['SESSION_DRIVER'] = 'cookie';
